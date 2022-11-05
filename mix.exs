f88432b974fdef19d0fb725123bedfae9128dad2
Artem Baikov <ivankolga25@gmail.com>
2022-11-05 15:44:47 +0300

    Removed unused deps and prepared package for publishing

--- mix.exs
+++ mix.exs
@@ -1,23 +1,30 @@
 defmodule EctoJuno.MixProject do
   use Mix.Project
 
+  @source_url "https://github.com/senconscious/ecto_juno"
+  @version "0.1.0"
+
   def project do
     [
       app: :ecto_juno,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      test_coverage: [ignore_modules: [EctoJuno.Schemas.User]]
+      test_coverage: [ignore_modules: [EctoJuno.Schemas.User]],
+      description: "A simple query sorting library for Elixir",
+      package: package(),
+      source_url: @source_url,
+      docs: [
+        extras: ["README.md"]
+      ]
     ]
   end
 
   # Run "mix help compile.app" to learn about applications.
   def application do
-    [
-      extra_applications: [:logger]
-    ]
+    []
   end
 
   # Run "mix help deps" to learn about dependencies.
@@ -25,7 +32,6 @@ defmodule EctoJuno.MixProject do
     [
       {:ecto, "~> 3.8"},
       {:ecto_sql, "~> 3.0"},
-      {:postgrex, ">= 0.0.0", only: [:test]},
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
       {:ex_doc, "~> 0.27", only: :dev, runtime: false}
     ]
@@ -34,4 +40,13 @@ defmodule EctoJuno.MixProject do
   # Specifies which paths to compile per environment.
   defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
+
+  defp package() do
+    [
+      files: ~w(lib priv .formatter.exs mix.exs README* readme* LICENSE*
+                license* CHANGELOG* changelog* src),
+      licenses: ["MIT"],
+      links: %{"GitHub" => @source_url}
+    ]
+  end
 end
